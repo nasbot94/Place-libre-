a684493c57d1f0a9d63415b7e384a6946bab3bb8
nasbot94 <nas.lakehal@gmail.com>
2026-09-18 08:56:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'placelibre-v1';
+const CACHE = 'placelibre-v2';
 const ASSETS = ['./placelibre.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
@@ -13,8 +13,14 @@ self.addEventListener('activate', (e) => {
   self.clients.claim();
 });
 
+// Réseau en priorité (toujours la dernière version tant qu'il y a du réseau),
+// on ne se rabat sur le cache que hors-ligne.
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => caches.match('./placelibre.html')))
+    fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request).then((cached) => cached || caches.match('./placelibre.html')))
   );
 });
